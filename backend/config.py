59abc064d185c3e62532833d88fea4be975fba94
Pythios <92983366+Thessalonicaa@users.noreply.github.com>
2026-01-21 13:20:57 +0700

    e

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,5 @@ class Config:
         'host': 'localhost',
         'port': 27017
     }
-    JWT_SECRET_KEY = "your-secret-key"  # ต้องเปลี่ยนเป็น key ที่ปลอดภัย
+    JWT_SECRET_KEY = "Zavora_8f3c9a2e6b7d4f1cA9E2D5K7M4R8Q6W"  # ต้องเปลี่ยนเป็น key ที่ปลอดภัย
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
